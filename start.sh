7cb30930f7c2bbbe5c6fc924a4603e30e98dba95
boxsie <dan@boxsie.co.uk>
2021-02-27 10:42:21 +0000

    updated to use a pvc

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
+if [ "$(ls -A /minecraft/world)" ]; then
+    echo "Minecraft world exitsts"
+else
+    echo "Downloading Minecraft world"
+    gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
+    gsutil cp gs://${BUCKET_PATH}/${WORLD_ZIP} /minecraft/${WORLD_ZIP}
+    gsutil cp gs://${BUCKET_PATH}/server.properties /minecraft/server.properties
+    gsutil cp gs://${BUCKET_PATH}/whitelist.json /minecraft/whitelist.json
 
-gsutil cp gs://${BUCKET_PATH}/${WORLD_ZIP} /minecraft/${WORLD_ZIP}
-gsutil cp gs://${BUCKET_PATH}/server.properties /minecraft/server.properties
-gsutil cp gs://${BUCKET_PATH}/whitelist.json /minecraft/whitelist.json
-
-unzip /minecraft/${WORLD_ZIP} -d /minecraft
+    unzip /minecraft/${WORLD_ZIP} -d /minecraft
+fi
 
 java -Xmx1024M -Xms1024M -XX:+UseG1GC -jar spigot-1.16.5.jar nogui
